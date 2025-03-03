078b1d3591ae1ba7cd0c1b72b34f5f139ed2aa5e
Brixxtine <kristinemariepf@gmail.com>
2025-03-03 11:49:50 +0800

    Version 3

--- script.js
+++ script.js
@@ -128,3 +128,45 @@ document.addEventListener("DOMContentLoaded", function() {
     loadSavedTasks();
     updateTaskApp();
 });
+
+const body = document.querySelector('body');
+const button = document.querySelector('.button');
+const icon = document.querySelector('.button__icon');
+
+function store(value){
+    localStorage.setItem('darkmode', value);
+}
+
+function load(){
+    const darkmode = localStorage.getItem('darkmode');
+
+    if(!darkmode){
+        store(false);
+    } else if (darkmode == 'true'){
+        body.classList.add('darkmode');
+        icon.classList.add('fa-moon');
+    }
+}
+
+load();
+
+button.addEventListener('click', () => {
+
+    body.classList.toggle('darkmode');
+    icon.classList.add('animated');
+
+    store(body.classList.contains('darkmode'));
+
+    if(body.classList.contains('darkmode')){
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
+
+    setTimeout( () => {
+        icon.classList.remove('animated');
+    }, 500)
+
+    })
